cee7093e38267f60a5a8408d03892f2d2fb918ca
urb4n3 <eliastilrem@gmail.com>
2025-03-04 08:25:32 +0100

    feature: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+      </div>
+      <h1 className="text-3xl font-bold underline">MikuCrypt!</h1>
+    </>
+  )
+}
+
+export default App
